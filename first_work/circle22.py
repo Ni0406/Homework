942b5bab0f55ca64e32724d453a8a42facf5ad8f
Ni0406 <ncuhlancev27@gmail.com>
2023-09-23 11:54:58 +0300

    secondtask

--- first_work/circle22.py
+++
@@ -1,7 +0,0 @@
-rad_int:  int = int(input("Radius\n"))
-per_float: float = float(input("П\n"))
-
-lenght = 2*rad_int*per_float
-square = per_float*rad_int**2
-
-print(f"{lenght} длина\n{square} площадь")
